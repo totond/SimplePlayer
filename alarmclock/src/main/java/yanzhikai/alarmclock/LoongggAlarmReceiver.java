242b440de3d86f5a98dc1cc97615108c1b7ab5a4
yany <yanzhikai_yjk@qq.com>
2018-02-23 09:46:58 +0800

    加入AlarmClock模块

---
+++ alarmclock/src/main/java/yanzhikai/alarmclock/LoongggAlarmReceiver.java
@@ -0,0 +1,35 @@
+package yanzhikai.alarmclock;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Vibrator;
+import android.util.Log;
+
+/**
+ * Created by loongggdroid on 2016/3/21.
+ */
+public class LoongggAlarmReceiver extends BroadcastReceiver {
+    private MediaPlayer mediaPlayer;
+    private Vibrator vibrator;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String msg = intent.getStringExtra("msg");
+        long intervalMillis = intent.getLongExtra("intervalMillis", 0);
+        Log.d("yjk", "onReceive:intervalMillis " + intervalMillis);
+        if (intervalMillis != 0) {
+            AlarmManagerUtil.setAlarmTime(context, System.currentTimeMillis() + intervalMillis,
+                    intent);
+        }
+        int flag = intent.getIntExtra("soundOrVibrator", 0);
+        Intent clockIntent = new Intent(context, ClockAlarmActivity.class);
+        clockIntent.putExtra("msg", msg);
+        clockIntent.putExtra("flag", flag);
+        clockIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(clockIntent);
+    }
+
+
+}
